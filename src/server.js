52d7c9dcdb325a59097423efc214c4365eea1f3a
kyoraki <luanharaki@gmail.com>
2024-02-27 23:18:16 -0300

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 dotenv.config();
 
 const server = express();
-const PORT = process.env.MYSQLPORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.use(express.json());
 server.use(cors(
