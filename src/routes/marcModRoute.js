50a1f192482650f9c16400eb0dcb68f8e0b5d9da
kyoraki <luanharaki@gmail.com>
2024-03-18 16:49:09 -0300

    deleted:    src/controllers/marcmodController.js 	deleted:    src/routes/marcModRoute.js 	modified:   src/server.js

--- src/routes/marcModRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getMarcMod } from "../controllers/marcmodController.js";
-
-const marcModRouter = express.Router();
-
-marcModRouter.get("/marcmod", getMarcMod);
-
-export default marcModRouter;
